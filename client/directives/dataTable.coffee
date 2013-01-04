9ae04114d1283f67b0897a31bc8a1d3a929f0f2c
David Bochenski <david@bochenski.co.uk>
2013-01-04 18:46:50 +0000

    working baseline 2

--- client/directives/dataTable.coffee
+++ client/directives/dataTable.coffee
@@ -1,11 +1,82 @@
 angular.module('app').directive 'datatable'
-, [ () ->
+, [ '$filter', ($filter) ->
   restrict: 'E'
   templateUrl: '/views/gint-ui/dataTable.html'
   transclude: true
-  scope: {}
+  scope:
+    items: '='
+  link: ($scope, element, attrs) ->
+    $scope.$watch 'items.length', (val, old_val) ->
+      $scope.search()
+
+    $scope.filteredItems = []
+    $scope.groupedItems = []
+    $scope.itemsPerPage = 20
+    $scope.pagedItems = []
+    $scope.currentPage = 0
+
+    $scope.things = $scope.items
+
+    $scope.search = ->
+      console.log ' datatable scope search'
+      $scope.filteredItems = $filter('filter')($scope.items, (item) ->
+        #we're not searching for anything - so return true
+        if not $scope.query
+          return true
+
+        #check if we're searching for a day string or if the query is in the title
+        if $filter('lowercase')(item.name)
+        .indexOf($filter('lowercase')($scope.query)) != -1
+          return true
+
+        return false
+      )
+
+      #sort the items before they go for pagination
+      $scope.filteredItems = $filter('orderBy')($scope.filteredItems
+      , (item) ->
+        item.name
+      , false )
+
+      $scope.currentPage = 0
+      # now group by pages
+      $scope.groupToPages()
+
+    # calculate page in place
+    $scope.groupToPages = ->
+      $scope.pagedItems = []
+      for thing, i in $scope.filteredItems
+        if (i % $scope.itemsPerPage == 0)
+          $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] =
+            [ $scope.filteredItems[i] ]
+        else
+          $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)]
+          .push($scope.filteredItems[i])
+
+    $scope.range = (start, end) ->
+      ret = []
+      if not end
+        end = start
+        start = 0
+
+      for num in [start..end]
+        ret.push num
+      ret
+
+    $scope.prevPage = ->
+      if $scope.currentPage > 0
+        $scope.currentPage = $scope.currentPage - 1
+
+    $scope.nextPage = ->
+      if $scope.currentPage < $scope.pagedItems.length - 1
+        $scope.currentPage = $scope.currentPage + 1
+
+    $scope.setPage = (n) ->
+      $scope.currentPage = n
+
+    $scope.search()
   controller: ['$scope', '$element', '$transclude'
-  ,($scope, $element, $transclude) ->
+  , ($scope, $element, $transclude) ->
     console.log 'datatable scope id = ' + $scope.$id
     console.log 'datatable parent scope id = ' + $scope.$parent.$id
     $transclude (clone) ->
--- client/index.js
+++ client/index.js
@@ -29,12 +29,83 @@ angular.module('app').directive('modal', [
 
 
 angular.module('app').directive('datatable', [
-  function() {
+  '$filter', function($filter) {
     return {
       restrict: 'E',
       templateUrl: '/views/gint-ui/dataTable.html',
       transclude: true,
-      scope: {},
+      scope: {
+        items: '='
+      },
+      link: function($scope, element, attrs) {
+        $scope.$watch('items.length', function(val, old_val) {
+          return $scope.search();
+        });
+        $scope.filteredItems = [];
+        $scope.groupedItems = [];
+        $scope.itemsPerPage = 20;
+        $scope.pagedItems = [];
+        $scope.currentPage = 0;
+        $scope.things = $scope.items;
+        $scope.search = function() {
+          console.log(' datatable scope search');
+          $scope.filteredItems = $filter('filter')($scope.items, function(item) {
+            if (!$scope.query) {
+              return true;
+            }
+            if ($filter('lowercase')(item.name).indexOf($filter('lowercase')($scope.query)) !== -1) {
+              return true;
+            }
+            return false;
+          });
+          $scope.filteredItems = $filter('orderBy')($scope.filteredItems, function(item) {
+            return item.name;
+          }, false);
+          $scope.currentPage = 0;
+          return $scope.groupToPages();
+        };
+        $scope.groupToPages = function() {
+          var i, thing, _i, _len, _ref, _results;
+          $scope.pagedItems = [];
+          _ref = $scope.filteredItems;
+          _results = [];
+          for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+            thing = _ref[i];
+            if (i % $scope.itemsPerPage === 0) {
+              _results.push($scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [$scope.filteredItems[i]]);
+            } else {
+              _results.push($scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]));
+            }
+          }
+          return _results;
+        };
+        $scope.range = function(start, end) {
+          var num, ret, _i;
+          ret = [];
+          if (!end) {
+            end = start;
+            start = 0;
+          }
+          for (num = _i = start; start <= end ? _i <= end : _i >= end; num = start <= end ? ++_i : --_i) {
+            ret.push(num);
+          }
+          return ret;
+        };
+        $scope.prevPage = function() {
+          if ($scope.currentPage > 0) {
+            return $scope.currentPage = $scope.currentPage - 1;
+          }
+        };
+        $scope.nextPage = function() {
+          if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            return $scope.currentPage = $scope.currentPage + 1;
+          }
+        };
+        $scope.setPage = function(n) {
+          return $scope.currentPage = n;
+        };
+        return $scope.search();
+      },
       controller: [
         '$scope', '$element', '$transclude', function($scope, $element, $transclude) {
           console.log('datatable scope id = ' + $scope.$id);
